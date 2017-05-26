9750142bd7a5d2f2bb4ec1ba3daa96d15e132261
Tom G <tgurchiek@gurchiekconsulting.com>
2017-05-26 10:28:57 -0400

    Added bootstrap gem along with the sample dashboard. This will continue to be modified. Made dashboard the root route for now.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  #get 'dashboard/index'
+
+  root to: 'dashboard#index'
+  # Need to set root_to the login page.
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
